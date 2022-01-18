023f9d1a699811943a73b3fc8ae43186288d380f
self-supervisor <augustine.mavor-parker.15@ucl.ac.uk>
2022-01-18 23:46:46 +0000

    wandb scraper

---
+++ scraper.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import wandb
+import math
+import matplotlib.pyplot as plt 
+
+def grab_runs_based_on_names(runs, name_list, x_name, y_name):
+    filtered_runs = []
+    for a_run in runs:
+        if a_run.name in name_list:
+            history = a_run.history()
+            x = history[x_name].values.tolist()
+            y = history[y_name].values.tolist()
+            filtered_runs.append([x, y])
+    return filtered_runs
+
+def filter_out_nans(run_list):
+    all_filtered_runs = []
+    for a_run_xy in run_list:
+        run_without_nan_x = []
+        run_without_nan_y = []
+        nan = False
+        for index, possible_nan in enumerate(a_run_xy[1]):
+            if math.isnan(possible_nan) == False:
+                run_without_nan_x.append(a_run_xy[0][index])
+                run_without_nan_y.append(a_run_xy[1][index])
+        all_filtered_runs.append([run_without_nan_x, run_without_nan_y])
+    return all_filtered_runs
+
+def compute_line_equation(x1, y1, x2, y2):
+    m = (y2 - y1)/(x2 - x1)
+    c = y2 - m * x2 
+    return m, c
+
+def compute_y_value(x, m, c):
+    return m * x + c
+
+def sample_x_points(a_filtered_run, number_of_points = 1000):
+    max_x = max(a_filtered_run)
+    min_x = min(a_filtered_run)
+    interval = (max_x - min_x)/number_of_points
+    x_points = [i * interval for i in range(number_of_points)]
+    return x_points
+
+def make_filtered_runs_have_same_points(runs, x_points):
+    same_point_runs = []
+    for a_run in runs:     
+        y_points = []
+        for a_x in x_points:
+            for index, x_value in enumerate(a_run[0]):
+                if x_value > a_x: 
+                    x1 = a_run[0][index - 1] 
+                    y1 = a_run[1][index - 1]
+                    x2 = a_run[0][index] 
+                    y2 = a_run[1][index]
+                    m, c = compute_line_equation(x1, y1, x2, y2)
+                    y_value = a_x * m + c
+                    y_points.append(y_value)
+                    break
+        same_point_runs.append([x_points, y_points])
+    return same_point_runs
+
+def main():
+    api = wandb.Api()
+    entity, project = "self-supervisor", "minigrid"  # set to your entity and project
+    runs = api.runs(entity + "/" + project)
+    curious_true_uncertainty_true_noisy_true_4_rooms = [
+        "blooming-glade-1549",
+        "drawn-pond-1551",
+        "lyric-firefly-1553",
+        "comfy-pond-1555",
+        "breezy-frog-1557",
+    ]
+    curious_true_uncertainty_true_noisy_true_4_rooms = grab_runs_based_on_names(
+        runs=runs,
+        name_list=curious_true_uncertainty_true_noisy_true_4_rooms,
+        x_name="_step",
+        y_name="fixed visiation counts",
+    )
+    curious_true_uncertainty_true_noisy_true_4_rooms = filter_out_nans(curious_true_uncertainty_true_noisy_true_4_rooms)
+    x_points = sample_x_points(curious_true_uncertainty_true_noisy_true_4_rooms[0][0])
+    curious_true_uncertainty_true_noisy_true_4_rooms = make_filtered_runs_have_same_points(curious_true_uncertainty_true_noisy_true_4_rooms, x_points) 
+    mean_line = 
+    plt.plot(curious_true_uncertainty_true_noisy_true_4_rooms[0][0], curious_true_uncertainty_true_noisy_true_4_rooms[0][1])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
